7a1d4f31bb754ea306b948d4ee61bae37a215412
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2023-12-04 09:09:11 +1100

    added java script

--- script.js
+++ script.js
@@ -1,29 +1,60 @@
 // Identify variables
+var wins = 0;
+var losses = 0;
+var ties = 0;
 
 var options = ["R", "P", "S"];
 
-// Prompt the user to enter a choice from the 3 options
-var userChoice;
+// Function of the game
+var playGame = function () {
+    var userChoice = window.prompt("Enter R, P, S:");
 
-// Computer to pick an option 
-// Find a way to randomly select an option as well
-// i = 0, 1, 2
+    // If the user makes choice not to play
+    if (!userChoice) {
+        console.log("user abandoned");
+    }
 
-var randomIndex = ;
-var computerChoice = options[randomIndex];
+    // Convert r, p, s to capitals if the user enters it wrong, removing the case sensitivity
+    userChoice = userChoice.toUpperCase()
+    console.log(userChoice);
 
-// You are alerting the user that computer has made a selection
+    // Computer selects at random
+    var index = Math.floor(Math.random() * options.length);
+    var computerChoice = options[index];
 
-if (userChoice===computerChoice) {
-    // alert on tie
-} else if (
-    (userChoice === "R" && computerChoice ==="S") ||
-    (userChoice === "P" && computerChoice ==="R") ||
-    (userChoice === "S" && computerChoice ==="P")
-) {
-    // user wins
-    // alert
-} else {
-    // user loose
-    // alert
+    // Alert the user the computer has made a selection
+    window.alert("The computer chose: " + computerChoice);
+
+    //Outcomes
+    if (userChoice===computerChoice) {
+        // alert on tie
+        ties++;
+        window.alert("Tie");
+    } else if (
+        (userChoice === "R" && computerChoice ==="S") ||
+        (userChoice === "P" && computerChoice ==="R") ||
+        (userChoice === "S" && computerChoice ==="P")
+    ) {
+        // user wins
+        // alert
+        wins++;
+        window.alert("You win! Congratulations!")
+    } else {
+        // user loose
+        // alert
+        losses++;
+        window.alert("You lose! Better luck next time!");
+    }
+
+    window.alert("Stats: \nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties);
+
+    // Play again alert
+    var playAgain = window.confirm("Play Again?");
+   
+
+    if (playAgain) {
+        playGame ();
+    }
 }
+
+playGame ();
